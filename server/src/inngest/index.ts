d3cf3b0db29ac0a20b957d7322188503f116f7ab
Anish Ghimire <pnoob189@gmail.com>
2025-10-28 10:32:12 +0545

    tiny changes

--- server/src/inngest/index.ts
+++ server/src/inngest/index.ts
@@ -9,36 +9,38 @@ export const inngest = new Inngest({ id: "slack_clone" });
 const syncUserProfile = inngest.createFunction(
     { id: "sync_user_profile" },
     { event: "clerk/user.created" },
-    async ({ event, step }) => {
-        const { id, email_addresses, first_name, last_name, image_url } = event.data;
-        const newUser = {
-            clerkId: id,
-            email: email_addresses[0].email_address,
-            name: `${first_name} ${last_name}`,
-            image: image_url,
-        };
-        try {
-            let db = connectDb();
-            await db.insert(usersTable).values(newUser);
-        } catch (error) {
-            console.error("Error inserting user into database:", error);
-            throw error;
-        }
+    async ({ event }) => {
+        console.log("🔔 Inngest function triggered", event.name, event.data);
+        // const { id, email_addresses, first_name, last_name, image_url } = event.data;
+        // const newUser = {
+        //     clerkId: id,
+        //     email: email_addresses[0].email_address,
+        //     name: `${first_name} ${last_name}`,
+        //     image: image_url,
+        // };
+        // try {
+        //     let db = connectDb();
+        //     await db.insert(usersTable).values(newUser);
+        // } catch (error) {
+        //     console.error("Error inserting user into database:", error);
+        //     throw error;
+        // }
     });
 
 
 const deleteUserProfile = inngest.createFunction(
     { id: "delete_user_profile" },
     { event: "clerk/user.deleted" },
-    async ({ event, step }) => {
-        const { id } = event.data;
-        try {
-            let db = connectDb();
-            await db.delete(usersTable).where(eq(usersTable.clerkId, id));
-        } catch (error) {
-            console.error("Error deleting user from database:", error);
-            throw error;
-        }
+    async ({ event }) => {
+        console.log("🔔 Inngest function triggered", event.name, event.data);
+        // const { id } = event.data;
+        // try {
+        //     let db = connectDb();
+        //     await db.delete(usersTable).where(eq(usersTable.clerkId, id));
+        // } catch (error) {
+        //     console.error("Error deleting user from database:", error);
+        //     throw error;
+        // }
     });
 
 // Export the functions to be used in the server
