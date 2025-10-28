4f66d49fdd4f4560998c8961c51cae00b281d4d4
Anish Ghimire <pnoob189@gmail.com>
2025-10-28 10:37:37 +0545

    tiny changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,13 @@ import { Hono } from 'hono';
 import { clerkMiddleware } from '@hono/clerk-auth';
 import { serve as inngestServe } from "inngest/hono";
 import { inngest, functions } from "./inngest/index.js";
+import { cors } from 'hono/cors';
 
 
 
 
 const app = new Hono();
+app.use("*",cors());
 app.use("/protected/*", clerkMiddleware());
 app.use("/api/inngest", inngestServe({ client: inngest, functions }));
 
--- server/src/inngest/index.ts
+++ server/src/inngest/index.ts
@@ -10,37 +10,36 @@ const syncUserProfile = inngest.createFunction(
     { id: "sync_user_profile" },
     { event: "clerk/user.created" },
     async ({ event }) => {
-        console.log("🔔 Inngest function triggered", event.name, event.data);
-        // const { id, email_addresses, first_name, last_name, image_url } = event.data;
-        // const newUser = {
-        //     clerkId: id,
-        //     email: email_addresses[0].email_address,
-        //     name: `${first_name} ${last_name}`,
-        //     image: image_url,
-        // };
-        // try {
-        //     let db = connectDb();
-        //     await db.insert(usersTable).values(newUser);
-        // } catch (error) {
-        //     console.error("Error inserting user into database:", error);
-        //     throw error;
-        // }
+        
+        const { id, email_addresses, first_name, last_name, image_url } = event.data;
+        const newUser = {
+            clerkId: id,
+            email: email_addresses[0].email_address,
+            name: `${first_name} ${last_name}`,
+            image: image_url,
+        };
+        try {
+            let db = connectDb();
+            await db.insert(usersTable).values(newUser);
+        } catch (error) {
+            console.error("Error inserting user into database:", error);
+            throw error;
+        }
     });
 
 
 const deleteUserProfile = inngest.createFunction(
     { id: "delete_user_profile" },
     { event: "clerk/user.deleted" },
-    async ({ event }) => {
-        console.log("🔔 Inngest function triggered", event.name, event.data);
-        // const { id } = event.data;
-        // try {
-        //     let db = connectDb();
-        //     await db.delete(usersTable).where(eq(usersTable.clerkId, id));
-        // } catch (error) {
-        //     console.error("Error deleting user from database:", error);
-        //     throw error;
-        // }
+    async ({ event, step }) => {
+        const { id } = event.data;
+        try {
+            let db = connectDb();
+            await db.delete(usersTable).where(eq(usersTable.clerkId, id));
+        } catch (error) {
+            console.error("Error deleting user from database:", error);
+            throw error;
+        }
     });
 
 // Export the functions to be used in the server
